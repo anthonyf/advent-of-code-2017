e96edc01d7bd8d638b7ae9d4d183e234c448278e
Anthony Fairchild <anthony.fairchild@tapad.com>
2017-12-08 14:38:08 -0500

    scala Day 1

---
+++ scala/src/main/scala/com/af/aoc2017/Tests.sc
@@ -0,0 +1,11 @@
+import com.af.aoc2017.Day01
+
+Day01.test2
+
+Day01.solution
+
+
+
+
+
+
---
+++ scala/src/test/scala/Tests.scala
@@ -0,0 +1,6 @@
+import com.af.aoc2017.Day01
+import org.scalatest.{FlatSpec, Matchers}
+
+class Tests extends FlatSpec with Matchers {
+    Day01.solve(Day01.input) should equal(0)
+}
